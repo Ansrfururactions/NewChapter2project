15a7df715216f0d2b26d1e77d3811117379e360e
ansrfururactions <careybarberjrgmail.com>
2024-09-24 17:35:40 -0400

    mad changes

--- NewChapter2project.cpp
+++ NewChapter2project.cpp
@@ -2,10 +2,14 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	double totalparts = 16500, percentpurchasers = 0.15, amountcitruslikers = totalparts * percentpurchasers * 0.58;
+
+	cout << "amount of energy drink purchasers :" << percentpurchasers << endl;
+	cout << "amount of citrus likers:" << amountcitruslikers << endl;
 }
 
 
