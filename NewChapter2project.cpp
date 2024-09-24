73cde7b7542664c9838c0fce7a1f8b6888e9c6f5
ansrfururactions <careybarberjrgmail.com>
2024-09-24 15:45:29 -0400

    other one

--- NewChapter2project.cpp
+++ NewChapter2project.cpp
@@ -1,20 +1,14 @@
-// NewChapter2project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Carey Barber
+// 9/23/2024
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double gallon = 20, towndist = gallon * 23.5, highwaydist = gallon * 28.9;
+
+    std::cout << "the car can travel a distance of " << towndist << " miles in a town, and " << highwaydist << " miles on the highway." << endl;
+        return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
